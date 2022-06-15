569ed71b26113084d3b863710e8915d9b36b1c97
Himanshu Sharma <sdothimanshu@gmail.com>
2022-06-15 15:54:15 +0530

    Implemented Recycler View with Sample text
    Currently NO Click Listener is Applied

--- app/src/main/java/com/example/newsly/MainActivity.kt
+++ app/src/main/java/com/example/newsly/MainActivity.kt
@@ -13,6 +13,18 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
+        val dataList : ArrayList<String> = itemData()
+        val adapteR : NewsListAdapter = NewsListAdapter(dataList)
+        recyclerView.adapter = adapteR
 
+
+    }
+
+    private fun itemData() : ArrayList<String>{
+        val list = ArrayList<String>()
+        for(i in 0 until 100){
+            list.add("Item no $i")
+        }
+        return list
     }
 }
--- app/src/main/java/com/example/newsly/NewsListAdapter.kt
+++ app/src/main/java/com/example/newsly/NewsListAdapter.kt
@@ -1,25 +1,37 @@
 package com.example.newsly
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+// NewsListAdapter takes DATA in its constructor
+// Here For example i am taking a list of strings just to understand how recycler view works
+class NewsListAdapter(private val items : ArrayList<String>): RecyclerView.Adapter<NewsItemViewHolder>() {
 
-class NewsListAdapter(val items : ArrayList<String>): RecyclerView.Adapter<NewsItemViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsItemViewHolder {
-        TODO("Not yet implemented")
+        // This function will be called everytime a view of data is created
+        // This function returns the xml that we created for news item view holder
+        // But we can't directly return the xml as it is, firstafall it needs to be converted in VIEW
+        // and to do so we need LAYOUT INFLATER CLASS
+        // LAYOUT INFLATED CLASS IS USED TO CONVERT AN XML DOCUMENT TO VIEW
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent,false)
+        return NewsItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: NewsItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        // This is the function which binds the data (fetching from constructor) with the view (simply the view that we have created in xml file)
+        val currentData = items[position]
+        holder.titleView.text = currentData
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        // This method is called once to fetch the item count like how many items are going to be there and this function needs INTEGER in return
+        return items.count() // This will return the current count number of the data that we're receiving in the above CONSTRUCTOR
     }
 
 }
 
 class NewsItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val TitleView : TextView = itemView.findViewById(R.id.title)
+    val titleView : TextView = itemView.findViewById(R.id.title)
 }
