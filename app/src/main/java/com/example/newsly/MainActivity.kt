234b6f3d8f05361a1b2e2fc2e8365ae0e8d1d49f
Himanshu Sharma <sdothimanshu@gmail.com>
2022-06-26 17:59:28 +0530

    Final update
    Added Glide library , Custom Chrome tab so that we can open url in our APP only , created news class to fetch all the data.

--- app/src/main/java/com/example/newsly/MainActivity.kt
+++ app/src/main/java/com/example/newsly/MainActivity.kt
@@ -1,34 +1,70 @@
 package com.example.newsly
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import org.json.JSONArray
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() , NewsItemClicked {
+
+    private lateinit var mAdapter : NewsListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // val recyclerView : RecyclerView = findViewById(R.id.recyclerView)
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
-        val dataList : ArrayList<String> = itemData()
-        val adapteR : NewsListAdapter = NewsListAdapter(dataList,this)
-        recyclerView.adapter = adapteR
+        itemData()
+        mAdapter = NewsListAdapter(this)
+        recyclerView.adapter = mAdapter
 
     }
 
-    private fun itemData() : ArrayList<String>{
-        val list = ArrayList<String>()
-        for(i in 100 until 200){
-            list.add("Item no $i")
-        }
-        return list
+    private fun itemData(){
+
+        val url = "https://saurav.tech/NewsAPI/top-headlines/category/health/in.json"
+
+        // Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url,null,
+            {
+                try {
+                    val newsArrayList: JSONArray = it.getJSONArray("articles")
+                    val list = ArrayList<News>()
+                    for(i in 0 until newsArrayList.length()){
+                        val newsJsonObject : JSONObject = newsArrayList.getJSONObject(i)
+                        val news = News(newsJsonObject.getString("title"),
+                            newsJsonObject.getString("author"),newsJsonObject.getString("url"),newsJsonObject.getString("urlToImage"))
+
+                        list.add(news)
+                    }
+
+                    mAdapter.updateListData(list)
+
+                }catch (e: Exception){
+                    e.printStackTrace()
+                }
+            },
+            { Log.d("error",it.localizedMessage) })
+        // Add the request to the RequestQueue.
+        // queue.add(jsonObjectRequest)     --> Nor this
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
-    override fun onItemClicked(item: String) {
-        Toast.makeText(this,"Clicked item is $item",Toast.LENGTH_SHORT).show()
+
+    override fun onItemClicked(item: News) {
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(item.url))
     }
 }
